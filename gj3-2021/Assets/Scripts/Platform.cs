d4318ab525734e610aa2d55a7f0b8a8d9162982d
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2021-03-05 21:26:02 -0600

    Platform test scene

---
+++ gj3-2021/Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Platform : MonoBehaviour
+{
+    [SerializeField] private Transform start = null;
+    [SerializeField] private Transform end = null;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetProgress(float progress)
+    {
+        rb.MovePosition(Vector2.Lerp(start.position, end.position, progress));
+    }
+}
