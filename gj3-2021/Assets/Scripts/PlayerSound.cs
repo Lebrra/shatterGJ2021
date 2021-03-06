423fece428f9aee59bcb226c2480558bc5e33e74
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2021-03-06 16:34:36 -0600

    PlayerSound script

---
+++ gj3-2021/Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] hits = null;
+    [SerializeField] private AudioClip[] shocks = null;
+    [SerializeField] private AudioClip[] falls = null;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayHit()
+    {
+        audioSource.PlayOneShot(hits[Random.Range(0, hits.Length)]);
+    }
+
+    public void PlayShock()
+    {
+        audioSource.PlayOneShot(shocks[Random.Range(0, shocks.Length)]);
+    }
+
+    public void PlayFall()
+    {
+        audioSource.PlayOneShot(shocks[Random.Range(0, falls.Length)]);
+    }
+}
