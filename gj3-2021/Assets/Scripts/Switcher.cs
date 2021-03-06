df167f9412cc4b62590671f49c64b4afb5fba870
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2021-03-05 20:28:30 -0600

    Switch test scene

---
+++ gj3-2021/Assets/Scripts/Switcher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Switcher : MonoBehaviour
+{
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        transform.GetChild(currentIndex).gameObject.SetActive(true);
+    }
+
+    public void SwitchTo(float index)
+    {
+        SwitchTo((int)index);
+    }
+
+    public void SwitchTo(int index)
+    {
+        if(currentIndex != index)
+        {
+            transform.GetChild(currentIndex).gameObject.SetActive(false);
+            transform.GetChild(index).gameObject.SetActive(true);
+            currentIndex = index;
+        }
+    }
+}
