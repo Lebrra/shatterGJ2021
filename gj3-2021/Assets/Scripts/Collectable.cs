a713602fe5113e333d028ec32e8bbdfd00b99a04
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2021-03-06 12:07:41 -0600

    Collectables

---
+++ gj3-2021/Assets/Scripts/Collectable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] private CollectableCount collectableCount = null;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("Player"))
+        {
+            collectableCount++;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ gj3-2021/Assets/Scripts/CollectableCount.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class CollectableCount : MonoBehaviour
+{
+    private int total = 0;
+    private TextMeshProUGUI textMeshPro = null;
+
+    void Start()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+    }
+
+    public static CollectableCount operator ++(CollectableCount count)
+    {
+        count.total++;
+        count.textMeshPro.text = count.total.ToString();
+        return count;
+    }
+
+}
