32b158dcfcfcce766ca3004ce5d146d7908d320a
SebaldDM13 <64290663+SebaldDM13@users.noreply.github.com>
2021-03-06 01:56:23 -0600

    Phasing test scene

---
+++ gj3-2021/Assets/Scripts/PhaseSlider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class PhaseSlider : MonoBehaviour
+{
+    [SerializeField] Material phaseMaterial = null;
+    [SerializeField] float inTuneFrequency = 0.5f;
+    [SerializeField] float range = 0.2f;
+    private Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        ValueChangeCheck();
+    }
+
+    public void ValueChangeCheck()
+    {
+        float solidity = Mathf.Clamp01(1 - Mathf.Abs(inTuneFrequency - slider.value) / range);
+        phaseMaterial.SetFloat("_Solidity", solidity);
+    }
+}
